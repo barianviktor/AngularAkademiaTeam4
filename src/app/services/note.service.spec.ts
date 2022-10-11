89f7dd3d8e65de34466318c6aaa04d1d310f780a
barianviktor <viktor.barian@gmail.com>
2022-10-11 10:01:12 +0200

    note delete tests

--- src/app/services/note.service.spec.ts
+++ src/app/services/note.service.spec.ts
@@ -8,9 +8,30 @@ describe('NoteService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(NoteService);
+    service.noteList$.next([
+      {
+        id: 0,
+        content: 'adsadsadsadsadsa',
+        color: '#E2007E',
+        backgroundColor: '#323232',
+        create_at: '2022-01-01',
+      },
+      {
+        id: 1,
+        content: 'adsadsdadsafdfasfcxycdsadsa',
+        color: '#ffffff',
+        backgroundColor: '#f0cb3a',
+        create_at: '2022-01-01',
+      },
+    ]);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should delete an item from list of notes', () => {
+    service.deleteNote(0);
+    expect(service.noteList$.getValue().length).toBe(1);
+  });
+  it('should delete all item form list of notes', () => {
+    service.deleteAllNotes();
+    expect(service.noteList$.getValue().length).toBe(0);
   });
 });
