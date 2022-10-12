92da74fd055171cefa8cc0ef6b84ec6b2921389f
Barián <Viktor.Barian@t-systems.com>
2022-10-12 11:49:05 +0200

    fix

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class AuthenticationService {
-  isLogedIn: boolean = false;
+  isLogedIn: boolean = true;
   username: string = 'username';
   password: string = 'password';
 
