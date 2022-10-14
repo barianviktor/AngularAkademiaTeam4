bd7a09b61716b77377c4f353f5000fe1811c91a5
Freetze <babos1221@gmail.com>
2022-10-14 17:08:23 +0200

    angular material accordion

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -7,7 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 import { CdkAccordionModule } from '@angular/cdk/accordion';
-
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
@@ -31,7 +31,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     MatCardModule,
     MatIconModule,
     MatButtonToggleModule,
-
+    MatExpansionModule,
     CdkAccordionModule,
 
     MatToolbarModule,
