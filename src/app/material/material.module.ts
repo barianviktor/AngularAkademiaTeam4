b809396db2aa06e64a01d332b5ca47e257e12c7b
viktor <viktor.barian@gmail.com>
2022-10-14 16:49:13 +0200

    @fix@

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,13 +5,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-<<<<<<< HEAD
-import {CdkAccordionModule} from '@angular/cdk/accordion';
 
-=======
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
->>>>>>> bde2c60b2e1e413e6c33920d378a4fbee048ca7f
 
 @NgModule({
   declarations: [],
@@ -22,12 +20,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     MatCardModule,
     MatIconModule,
     MatButtonToggleModule,
-<<<<<<< HEAD
 
-=======
     MatToolbarModule,
     MatSidenavModule,
->>>>>>> bde2c60b2e1e413e6c33920d378a4fbee048ca7f
   ],
   exports: [
     MatButtonModule,
@@ -36,13 +31,11 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     MatCardModule,
     MatIconModule,
     MatButtonToggleModule,
-<<<<<<< HEAD
-    CdkAccordionModule
 
-=======
+    CdkAccordionModule,
+
     MatToolbarModule,
     MatSidenavModule,
->>>>>>> bde2c60b2e1e413e6c33920d378a4fbee048ca7f
   ],
 })
 export class MaterialModule {}
